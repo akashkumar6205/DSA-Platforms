a261cce5639a084c7ffd3949b2ad09887367a407
Akash kumar <akashkumar23012005@gmail.com>
2026-07-21 23:16:18 +0530

    Time: 1 ms (94.28%), Space: 43.7 MB (15.84%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -0,0 +1,34 @@
+class Solution {
+
+     public int nCr(int n, int r) {
+        r = Math.min(r, n - r);
+
+        long res = 1;
+
+        for (int i = 0; i < r; i++) {
+            res = res * (n - i);
+            res = res / (i + 1);
+        }
+
+        return (int) res;
+    }
+
+
+    public List<List<Integer>> generate(int numRows) {
+
+    List<List<Integer>> ans = new ArrayList<>();
+     for (int row = 1; row <= numRows; row++) {
+
+            List<Integer> temp = new ArrayList<>();
+
+            for (int col = 1; col <= row; col++) {
+                temp.add(nCr(row - 1, col - 1));
+            }
+
+            ans.add(temp);
+        }
+
+        return ans;
+        
+    }
+}
